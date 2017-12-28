1a064d795b5ea240236c1393316e2330b0d6881b
Pavel Khusainov <alvelcom@gmail.com>
2017-12-28 14:46:55 +0100

    Initial version of interpolation

---
+++ interpolation/interpolation.go
@@ -0,0 +1,83 @@
+package interpolation
+
+import (
+	"errors"
+
+	//aparser "github.com/mattn/anko/parser"
+	//avm "github.com/mattn/anko/vm"
+
+	"github.com/alvelcom/redoubt/api"
+)
+
+type Env struct {
+	Client *api.MachineInfo
+	Vars   map[string]interface{}
+}
+
+type String interface {
+	String(e *Env) (string, error)
+}
+
+type StringList interface {
+	StringList(e *Env) ([]string, error)
+}
+
+func StringVar(dst *String, i interface{}) error {
+	if i == nil {
+		*dst = nil
+		return nil
+	}
+
+	switch v := i.(type) {
+	case string:
+		s := constString(v)
+		*dst = &s
+		return nil
+	case int:
+		s := constString(string(v))
+		*dst = &s
+		return nil
+	default:
+		return errors.New("NewString: bad type")
+	}
+}
+
+func StringListVar(dst *StringList, i interface{}) error {
+	if i == nil {
+		*dst = nil
+		return nil
+	}
+
+	switch v := i.(type) {
+	case []interface{}:
+		l := make([]String, len(v))
+		*dst = stringList(l)
+		for j := range v {
+			if err := StringVar(&l[j], v[j]); err != nil {
+				return err
+			}
+		}
+		return nil
+	case string:
+		panic("string list is not impemented")
+	}
+	return nil
+}
+
+type constString string
+type stringList []String
+
+func (c *constString) String(e *Env) (string, error) {
+	return string(*c), nil
+}
+
+func (v stringList) StringList(e *Env) ([]string, error) {
+	out := make([]string, len(v))
+	for i := range v {
+		var err error
+		if out[i], err = v[i].String(e); err != nil {
+			return out, err
+		}
+	}
+	return out, nil
+}
--- producers/producers.go
+++ producers/producers.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 
 	"github.com/alvelcom/redoubt/config"
+	inter "github.com/alvelcom/redoubt/interpolation"
 )
 
 var ErrBadProducerType = errors.New("producers: bad type")
@@ -14,19 +15,15 @@ type Producer interface {
 
 // PKI
 type PKI struct {
-	Mount string
-	Role  string
-	CSR   CSR
+	Backend inter.String
+	Role    inter.String
+	CRT     CRT
 }
 
-type CSR struct {
-	CommonName string   `yaml:"common-name"`
-	AltDNS     []string `yaml:"alt-dns"`
-	AltIPs     []string `yaml:"alt-ip"`
-}
-
-// Secret
-type Secret struct {
+type CRT struct {
+	CommonName inter.String
+	AltDNS     inter.StringList
+	AltIPs     inter.StringList
 }
 
 func New(c config.Producer) (Producer, error) {
@@ -39,7 +36,40 @@ func New(c config.Producer) (Producer, error) {
 }
 
 func newPKI(c config.Producer) (Producer, error) {
-	return &PKI{}, nil
+	params, ok := c.Value.(map[interface{}]interface{})
+	if !ok {
+		return nil, errors.New("new pki: can't cast to map")
+	}
+
+	var pki PKI
+
+	if err := inter.StringVar(&pki.Backend, params["backend"]); err != nil {
+		return nil, err
+	}
+	if err := inter.StringVar(&pki.Role, params["role"]); err != nil {
+		return nil, err
+	}
+
+	crt_, ok := params["crt"]
+	if !ok {
+		return nil, errors.New("new pki: no crt block")
+	}
+	crt, ok := crt_.(map[interface{}]interface{})
+	if !ok {
+		return nil, errors.New("new pki: can't cast crt to map")
+	}
+
+	if err := inter.StringVar(&pki.CRT.CommonName, crt["common-name"]); err != nil {
+		return nil, err
+	}
+	if err := inter.StringListVar(&pki.CRT.AltDNS, crt["alt-dns"]); err != nil {
+		return nil, err
+	}
+	if err := inter.StringListVar(&pki.CRT.AltIPs, crt["alt-ips"]); err != nil {
+		return nil, err
+	}
+
+	return &pki, nil
 }
 
 func (p *PKI) Type() string {
